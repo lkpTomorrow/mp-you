dce7e85056c203f1c81a4696153da4aa7d06ab3b
likaipeng <lkpTomorrow@gmail.com>
2020-05-16 13:18:56 +0800

    feat(): 增加了antd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import {NzI18nService} from 'ng-zorro-antd';
 
+import getISOWeek from 'date-fns/get_iso_week';
+import { en_US, zh_CN, NzI18nService } from 'ng-zorro-antd';
 @Component({
   selector: 'mp-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'market-place';
+
+  date = null; // new Date();
+  dateRange = []; // [ new Date(), addDays(new Date(), 3) ];
+  isEnglish = false;
+
+  constructor(private i18n: NzI18nService) {}
+
+  onChange(result: Date): void {
+    console.log('onChange: ', result);
+  }
+
+  getWeek(result: Date): void {
+    console.log('week: ', getISOWeek(result));
+  }
+
+  changeLanguage(): void {
+    this.i18n.setLocale(this.isEnglish ? zh_CN : en_US);
+    this.isEnglish = !this.isEnglish;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import {FormsModule} from '@angular/forms';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+registerLocaleData(zh);
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+  FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    /** 导入 ng-zorro-antd 模块 **/
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers   : [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
